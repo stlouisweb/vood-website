997a2d2cd4bee6a1b3f2c2cc3e5b8dc20a92bf3d
stlouisweb <stlouisweb@gmail.com>
2017-07-09 08:27:40 -0500

    working on grid WIP.

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,11 +1,6 @@
 // @flow
 
-import bballimg from '../../assets/basketball.jpg';
-import bieberimg from '../../assets/bieber.jpg';
-import ellenimg from '../../assets/ellen.jpg';
-import oscarsimg from '../../assets/oscars.jpg';
 import React, {Component} from 'react';
-import transimg from '../../assets/transformers.jpg';
 import vrheadset from '../../assets/vrheadset.png';
 import './Categories.css';
 
@@ -19,73 +14,38 @@ class Categories extends Component {
           </h2>
         </div>
         <div className="centerImages">
-          <div className="container-fluid">
-            <div className="container row tranSize">
-              <div className="col-xs-12 colPadding twelve">
-                <img
-                  src={transimg}
-                  className="transformersImg"
-                  alt="transformers"
-                />
-                <h2>Movies</h2>
-                <img src={vrheadset} className="vrheadsetImg" alt="vrheadset" />
-              </div>
-            </div>
+          <div className="trans">
+            <h2>Movies</h2>
+            <img src={vrheadset} className="vrheadsetImg" alt="vrheadset" />
           </div>
-          <div className="container-fluid">
-            <div className="row imgWidth">
-              <div className="col-xs-4 center bballctr colPadding">
-                <h2>Sporting Events</h2>
-                <p>
-                  Be transported to sporting venues and be part of all the
-                  courtside action
-                </p>
-                <img src={bballimg} className="bballImg" alt="basketball" />
-                <img src={vrheadset} className="vrheadsetImg" alt="vrheadset" />
-              </div>
-              <div className="col-xs-8 center bieberctr colPadding">
-                <div className="bieberCenter">
-                  <h2>Live Concerts</h2>
-                  <p>Enjoy your favorite artists from the front row</p>
-                  <img src={bieberimg} className="bieberImg" alt="bieber" />
-                  <img
-                    src={vrheadset}
-                    className="vrheadsetImg"
-                    alt="vrheadset"
-                  />
-                </div>
-                <div className="container-fluid">
-                  <div className="row showWidth">
-                    <div className="col-xs-6 center ellenctr colPadding six">
-                      <h2>Talk Shows</h2>
-                      <p>
-                        Attend your favorite talk shows without without the
-                        frustration of limited seats
-                      </p>
-                      <img src={ellenimg} className="ellenImg" alt="ellen" />
-                      <img
-                        src={vrheadset}
-                        className="vrheadsetImg"
-                        alt="vrheadset"
-                      />
-                    </div>
-                    <div className="col-xs-6 center oscarctr colPadding six">
-                      <h2>Events</h2>
-                      <p>
-                        Be part of all the fun and entertainment of glamorous
-                        events. See your favorite celebrities close up.
-                      </p>
-                      <img src={oscarsimg} className="oscarsImg" alt="oscars" />
-                      <img
-                        src={vrheadset}
-                        className="vrheadsetImg"
-                        alt="vrheadset"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="bball">
+            <h2>Sporting Events</h2>
+            <p>
+              Be transported to sporting venues and be part of all the courtside
+              action
+            </p>
+            <img src={vrheadset} className="vrheadsetImg" alt="vrheadset" />
+          </div>
+          <div className="bieber">
+            <h2>Live Concerts</h2>
+            <p>Enjoy your favorite artists from the front row</p>
+            <img src={vrheadset} className="vrheadsetImg" alt="vrheadset" />
+          </div>
+          <div className="ellen">
+            <h2>Talk Shows</h2>
+            <p>
+              Attend your favorite talk shows without without the frustration of
+              limited seats
+            </p>
+            <img src={vrheadset} className="vrheadsetImg" alt="vrheadset" />
+          </div>
+          <div className="oscar">
+            <h2>Events</h2>
+            <p>
+              Be part of all the fun and entertainment of glamorous events. See
+              your favorite celebrities close up.
+            </p>
+            <img src={vrheadset} className="vrheadsetImg" alt="vrheadset" />
           </div>
         </div>
       </div>
